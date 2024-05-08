99d7c4680e9a65aa2dadd9d66f3fc3e864196c5d
utpolra1 <utpolra@gmail.com>
2024-05-08 11:20:39 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,103 @@
+const express = require("express");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 5000;
+
+//midleware
+app.use(cors());
+app.use(express.json());
+
+//impordant this line
+require("dotenv").config();
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3i9ecp5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+
+    const serviceCollection = client.db("carDoctor").collection("services");
+    const bookingCollection = client.db("carDoctor").collection("bookings");
+
+    // services related api
+    app.get("/services", async (req, res) => {
+      const cursor = serviceCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      console.log(id);
+      const cursor = await serviceCollection.findOne(query);
+      // const result = await cursor.toArray();
+      res.send(cursor);
+    });
+
+    // app.post('/bookings', async (req, res) => {
+    //     const booking = req.body;
+    //     console.log(booking);
+    //     const result = await bookingCollection.insertOne(booking);
+    //     res.send(result);
+    // });
+
+    // app.get('/bookings', async (req, res) => {
+    //     const cursor = bookingCollection.find(query).toArray();
+    //     res.send(result);
+    // })
+
+    // delte mathod
+    app.delete("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await serviceCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    //update mthod
+    app.put("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updateDoc = req.body;
+      const update = {
+        $set: {
+          title: updateDoc.title,
+          price: updateDoc.price,
+          description: updateDoc.description,
+        },
+      };
+      const result = await serviceCollection.updateOne(query, update, options);
+      res.send(result);
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Car-Doctor Server is Running");
+});
+app.listen(port, () => {
+  console.log(`Car doctor runging ${port}`);
+});
